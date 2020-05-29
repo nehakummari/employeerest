41b6357bd3ed7eaacdcc891cfecb759aaacc85e3
neha <nehakummari@gmail.com>
2020-05-29 22:12:21 +0530

    Initial commit

---
+++ employeerest/src/test/java/com/neha/rest/EmployeerestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neha.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
